e3e932a63b5ae992026a1e33b2eb103163507f21
BPrince <brockprince@gmail.com>
2017-01-27 02:27:23 +0000

    First Commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import socket
+
+## Get my machine hostname
+if socket.gethostname().find('.') >= 0:
+    hostname=socket.gethostname()
+else:
+    hostname=socket.gethostbyaddr(socket.gethostname())[0]
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hey IoT World from RPi3: " + hostname
+
+## Run the website and make sure to make
+##  it externally visible with 0.0.0.0:5000 (default port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
